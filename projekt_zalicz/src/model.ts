a161fa3ee87718e5f1ef02b8d3e35cd8021786fb
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-08-28 18:29:35 +0200

    created models

---
+++ projekt_zalicz/src/model.ts
@@ -0,0 +1,55 @@
+export interface Reservation {
+    Table_Id : string;
+    Time_Start : string;
+    Time_End : string
+    Client_Name : string
+}
+
+export interface Employee{
+    Name: string;
+    Surename: string;
+    Position: string;
+}
+
+export interface Dish{
+    Name: string;
+    Price: number;
+    Category: string;
+}
+
+export interface Table{
+    Name: string;
+    Capacity: number;
+    Status: string;
+    Order:{
+        Employee : {
+            Name: string;
+            Surename: string;
+            Position: string;
+        }
+        Dishes : [{
+            Name: string;
+            Price: number;
+            Category: string;
+        }]
+        Status: string;
+        Creation_date: string;
+        Bill: number; 
+    }
+}
+
+export interface Restaurant{
+    Name: string;
+    Address: string;
+    Phone: number;
+    nip: number;
+    email: string;
+    website: string;
+}
+
+export interface Product{
+    Name: string;
+    Price: number;
+    Quantity: number;
+    Measurement_Units: string;
+}
