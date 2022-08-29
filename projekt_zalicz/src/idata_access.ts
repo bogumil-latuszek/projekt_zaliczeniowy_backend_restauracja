f03b85f2576166f43e9fe4719091f2753a8943ac
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-08-29 03:32:32 +0200

    created idata_access

---
+++ projekt_zalicz/src/idata_access.ts
@@ -0,0 +1,67 @@
+import {Employee, Restaurant, Reservation, Table, Order, Product, Dish} from './model';
+
+interface IEmployeeAccess {
+    HasEmployee(id:string): Promise<boolean>;
+    GetEmployee(id:string): Promise<Employee | undefined>;
+    GetAllEmployees(): Promise<Employee[]>;
+    AddEmployee(Employee:Employee): Promise<Employee>;
+    UpdateEmployee(Employee:Employee): Promise<void>;
+    DeleteEmployee(id:string): Promise<void>;
+}
+
+interface IRestaurantAccess {
+    HasRestaurant(id:string): Promise<boolean>;
+    GetRestaurant(id:string): Promise<Restaurant | undefined>;
+    AddRestaurant(Restaurant:Restaurant): Promise<Restaurant>;
+    UpdateRestaurant(Restaurant:Restaurant): Promise<void>;
+    DeleteRestaurant(id:string): Promise<void>;
+}
+
+interface IReservationAccess {
+    HasReservation(id:string): Promise<boolean>;
+    GetReservation(id:string): Promise<Reservation | undefined>;
+    GetAllReservations(): Promise<Reservation[]>;
+    AddReservEmployeeation(Reservation:Reservation): Promise<Reservation>;
+    UpdateReservation(Reservation:Reservation): Promise<void>;
+    DeleteReservation(id:string): Promise<void>;
+}
+
+interface ITableAccess {
+    HasTable(id:string): Promise<boolean>;
+    GetTable(id:string): Promise<Table | undefined>;
+    GetAllTables(): Promise<Table[]>;
+    AddTable(Table:Table): Promise<Table>;
+    UpdateTable(Table:Table): Promise<void>;
+    DeleteTable(id:string): Promise<void>;
+    FindFreeTable(): Promise<Table | undefined>;
+    //RaportTables(): Promise<Table[]>;
+}
+
+interface IOrderAccess {
+    HasOrder(id:string): Promise<boolean>;
+    GetOrder(id:string): Promise<Order | undefined>;
+    GetAllOrders(): Promise<Order[]>;
+    AddOrder(Order:Order): Promise<Order>;
+    UpdateOrder(Order:Order): Promise<void>;
+    DeleteOrder(id:string): Promise<void>;
+    FindFreeOrder(): Promise<Order | undefined>;
+    //RaportOrders(): Promise<Order[]>;
+}
+
+interface IProductAccess {
+    HasProduct(id:string): Promise<boolean>;
+    GetProduct(id:string): Promise<Product | undefined>;
+    GetAllProducts(): Promise<Product[]>;
+    AddProduct(Product:Product): Promise<Product>;
+    UpdateProduct(Product:Product): Promise<void>;
+    DeleteProduct(id:string): Promise<void>;
+}
+
+interface IDishAccess {
+    HasDish(id:string): Promise<boolean>;
+    GetDish(id:string): Promise<Dish | undefined>;
+    GetAllDishes(): Promise<Dish[]>;
+    AddDish(Dish:Dish): Promise<Dish>;
+    UpdateDish(Dish:Dish): Promise<void>;
+    DeleteDish(id:string): Promise<void>;
+}
