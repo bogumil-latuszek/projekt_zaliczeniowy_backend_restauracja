a5375c78a4e676fa4415a0008e66f25a6f85c068
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-09-01 21:39:31 +0200

    created dish.test
    initial state of tests for Dish model (just connect/disconnect DB inside tests setup/teardown)

---
+++ projekt_zalicz/src/tests/dish.test.ts
@@ -0,0 +1,26 @@
+import {
+    setupDBConnection,
+    teardownDBConnection,
+} from "../db_data_access";
+/*
+import {
+    Dish
+} from "../model"
+import {
+    Mongo_Dish
+} from "../mongo_models"
+*/
+
+describe("dishModel Testing", () => {
+    beforeAll(() => {
+        setupDBConnection();
+    });
+  
+    afterAll(() => {
+        teardownDBConnection();
+    });
+})
+
+test('empty test', () => {
+    expect(1).toEqual(1)
+})
