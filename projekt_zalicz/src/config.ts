90f7360e4bcf74cfb4a07895e883a2976ae73665
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-09-02 01:39:41 +0200

    let config always find .config.json independently from CWD

--- projekt_zalicz/src/config.ts
+++ projekt_zalicz/src/config.ts
@@ -1,5 +1,10 @@
 import * as fs from 'fs';
+import path from 'path';
 
-const config_file_content = fs.readFileSync(".config.json", 'utf-8');
+// fs.readFileSync resolves file path relative to current working directory
+// const config_file_content = fs.readFileSync(".config.json", 'utf-8');
+
+// but we want to resolve starting from this file location:
+const config_file_content = fs.readFileSync(path.resolve(__dirname, '../.config.json'), 'utf-8');
 const config = JSON.parse(config_file_content)
 export default config;
