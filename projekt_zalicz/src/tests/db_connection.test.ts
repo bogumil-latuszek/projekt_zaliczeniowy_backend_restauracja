5ce881678279ed1a74fedad6c06ef02af9b23c67
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-09-03 10:02:55 +0200

    deleted redundant and unnecessary tests

--- projekt_zalicz/src/tests/db_connection.test.ts
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-
-test('connection test', async () =>{
-    await mongoose.connect('mongodb://localhost:27017/myapp')
-} )
--- projekt_zalicz/src/tests/reservation_add2.test.ts
+++
@@ -1,7 +0,0 @@
-import { DbReservation, setupDBConnection } from '../db_data_access'
-
-test('db connection test2', () =>{
-    setupDBConnection();
-    let db_reserv = new DbReservation;
-    expect(db_reserv.HasReservation("this is obviously not an id")).toEqual(false);
-} )
