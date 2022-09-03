73334f62fad7229d9f8aa5a9b2fd83eb3b8f2eed
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-09-03 13:06:23 +0200

    new tests for hasdish

--- projekt_zalicz/src/tests/dish.test.ts
+++ projekt_zalicz/src/tests/dish.test.ts
@@ -4,7 +4,7 @@ import {
 } from "../mongo_db_data_access";
 
 import { Dish } from "model"
-import { Mongo_Dish } from "mongo_models"
+import { Mongo_Dish, Mongo_Reservation } from "mongo_models"
 import { MongoDbDishes } from "mongo_db_data_access"
 import { getDishesAccess } from "data_access_selector"
 
@@ -15,8 +15,11 @@ describe("dishModel Testing", () => {
     });
   
     afterAll(async () => {
-        await Mongo_Dish.collection.drop();
         await disconnectDBForTesting();
+    });
+  
+    afterEach(async () => {
+        await Mongo_Dish.collection.drop();
     });
 
     test('test Mongo_Dish direct creation', async () => {
@@ -61,3 +64,30 @@ describe("dishModel Testing", () => {
           expect(createdDish.Category).toBe(sprite.Category);
     });
 })
+
+describe("HasDish Testing", () => {
+    let db_dish = getDishesAccess();
+    beforeAll(async () => {
+        await connectDBForTesting();
+    });
+  
+    afterAll(async () => {
+        await disconnectDBForTesting();
+    });
+  
+    afterEach(async () => {
+        await Mongo_Reservation.collection.drop();
+    });
+
+    test('HasDish returns false when asked for non existant id', async () =>{
+
+        let result = await db_dish.HasDish("56e6dd2eb4494ed008d595bd");
+        expect(result).toEqual(false);
+    });
+
+    /*test('HasDish returns error when asked for id with wrong structure', async () =>{
+
+        let result = await db_dish.HasDish("wrong");
+        expect(result).toEqual(false);
+    });*/
+});
