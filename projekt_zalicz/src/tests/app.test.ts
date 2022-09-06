e95edab366bd9fddaf641c33e7f20a6c02daac69
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-09-06 23:36:08 +0200

    created test for app

---
+++ projekt_zalicz/src/tests/app.test.ts
@@ -0,0 +1,11 @@
+const request = require("supertest")
+const baseURL = "http://localhost:3000"
+
+describe("GET /", () => {
+
+    it("should return 200 from application main URL", async () => {
+      const response = await request(baseURL).get("/");
+      expect(response.statusCode).toBe(200);
+    });
+
+});
