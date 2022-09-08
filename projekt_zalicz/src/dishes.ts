cf0b2e80ff93c9b180b836e51b9a5b1c5dbebfdb
Bogumil Latuszek <bogumil_latuszek@o2.pl>
2022-09-08 16:07:35 +0200

    fixed /dishes/ endpoint

--- projekt_zalicz/src/dishes.ts
+++ projekt_zalicz/src/dishes.ts
@@ -9,12 +9,12 @@ export default router
 
 let db_dish: IDishAccess = getDishesAccess();
 
-router.get('/dishes', async (req: Request, res: Response) => {
+router.get('/', async (req: Request, res: Response) => {
     let all_dishes: Dish[] = await db_dish.GetAllDishes();
     res.status(200).send(all_dishes);
 })
 
-router.get('/dish/:id', async (req: Request, res: Response) => {
+router.get('/:id', async (req: Request, res: Response) => {
     let searching_id = req.params.id
     let dish_exists = await db_dish.HasDish(searching_id);
     if(dish_exists){
@@ -26,19 +26,18 @@ router.get('/dish/:id', async (req: Request, res: Response) => {
     }
 })
 
-router.post('/dish', async(req:Request, res: Response) =>{
-    try{
+router.post('/', async(req:Request, res: Response) =>{
+    try {
         let dish: Dish = req.body
-        let newly_added_id = db_dish.AddDish(dish);
-        res.status(200).send(newly_added_id);
+        let newly_added_id = await db_dish.AddDish(dish);
+        res.status(201).send({'id': newly_added_id});
     }
     catch(err){
         res.status(500).send(`error: ${err.message}`);
     }
-    
 })
 
-router.put('/dish/:id', async(req:Request, res: Response) =>{
+router.put('/:id', async(req:Request, res: Response) =>{
     try{
         let searching_id = req.params.id
         let dish: Dish = req.body
@@ -49,19 +48,18 @@ router.put('/dish/:id', async(req:Request, res: Response) =>{
     }
     let searching_id = req.params.id
     let dish: Dish = req.body
-    let newly_added_id = await db_dish.UpdateDish(dish, searching_id);
-    res.status(200).send();
+    await db_dish.UpdateDish(dish, searching_id);
+    res.status(204).send({})  // 204 - no content
  })
 
- router.delete('/dish/:id', async(req:Request, res: Response) =>{
+ router.delete('/:id', async(req:Request, res: Response) =>{
     let searching_id = req.params.id
     let dish_exists = await db_dish.HasDish(searching_id);
     if(dish_exists){
         await db_dish.DeleteDish(searching_id);
-         res.status(200).send();
+        res.status(204).send({})  // 204 - no content
     }
     else{
         res.status(404).send(`dish with id=${searching_id} doesn't exist`);
     }
-    
  })
--- projekt_zalicz/src/tests/endpoint_dishes.test.ts
+++ projekt_zalicz/src/tests/endpoint_dishes.test.ts
@@ -1,56 +1,184 @@
-import request from "supertest"
+import { Dish } from 'model';
+import { initDataAccess, dropDataAccess, clearAllData } from 'data_access_selector'
+import request from 'supertest';
 const baseURL = "http://localhost:3000"
 
-
-//czy wszystkie testy mogą mieć te same nazwy
-//dlaczego zamiast test jest it
-// dawać afterall żeby jeden test nie przeszkadzał kolejnemu?
-//! pamiętaj żeby odpalać je przez osobny powershell po uruchomieniu start
-
-describe("GET /", () => {
-
-    it("should return 200 from application main URL", async () => {
-      const response = await request(baseURL).get("/");
-      expect(response.statusCode).toBe(200);
-    });
-
+beforeAll(async () => {
+    await initDataAccess();
 });
 
-describe("POST /", () => {
-
-    const dish = {
-        Name: "koktail pomarańczowy",
-        Price: 7,
-        Category: "desery",
-    }
-
-    it("should return 200 from application main URL", async () => {
-        
-      const response = await request(baseURL).post("/").send(dish);
-      expect(response.statusCode).toBe(200);
-    });
-
+afterAll(() => {
+    dropDataAccess();
 });
 
-describe("PUT /", () => {
+describe("GET /dishes/", () => {
+
+    afterAll(async () => {
+        await clearAllData();
+    });
+
+    it("should return 200 from dishes/ main URL", async () => {
+        const response = await request(baseURL).get("/dishes/");
+        expect(response.statusCode).toBe(200);
+    });
+
+    it("should return all existing dishes/", async () => {
+        //assume
+        const dish1: Dish = {
+            Name: "ogórkowa",
+            Price: 5,
+            Category : "zupy"
+        }
+        const dish2: Dish = {
+            Name: "grzybowa",
+            Price: 15,
+            Category: "zupy"
+        }
+        const response1 = await request(baseURL).post("/dishes/").send(dish1);
+        const response2 = await request(baseURL).post("/dishes/").send(dish2);
+
+        //act
+        const response = await request(baseURL).get("/dishes/");
+
+        //assert
+        expect(response.body.length).toBe(2)
+        expect(response.body).toContainEqual(expect.objectContaining(
+          {
+            _id: expect.anything(),
+            Name: "ogórkowa",
+            Price: 5,
+            Category : "zupy"
+          }
+        ))
+        expect(response.body).toContainEqual(expect.objectContaining(
+          {
+            _id: expect.anything(),
+            Name: "grzybowa",
+            Price: 15,
+            Category: "zupy"
+          }
+        ))
+    });
+});
+
+describe("GET /dishes/:id", () => {
+
+    afterAll(async () => {
+        await clearAllData();
+    });
+
+    it("should return dish for existing ID", async () => {
+        //assume
+        const dish1: Dish = {
+            Name: "ogórkowa",
+            Price: 5,
+            Category : "zupy"
+        }
+        const response1 = await request(baseURL).post("/dishes/").send(dish1);
+        const dish_id = response1.body.id
+
+        //act
+        const response = await request(baseURL).get(`/dishes/${dish_id}`);
+
+        //assert
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual(expect.objectContaining(
+          {
+            _id: dish_id,
+            Name: "ogórkowa",
+            Price: 5,
+            Category : "zupy"
+          }
+        ))
+    });
+});
+
+describe("POST /dishes/", () => {
     //assume
-    const dish1 = {
-        Name: "koktail pomarańczowy",
-        Price: 7,
-        Category: "desery",
-    }
-    const dish2 = {
-        Name: "koktail jabłkowy",
+    const newDish: Dish = {
+        Name: "ogórkowa",
         Price: 5,
-        Category: "desery",
+        Category : "zupy"
     }
-    //act
 
-    it("should return 200 from application main URL", async () => {
-      let first_id = await request(baseURL).post("/").send(dish1);
-      const response = await request(baseURL).put("/").send(dish2);
-      expect(response.statusCode).toBe(200);
-      expect(response.)
+    afterAll(async () => {
+        await clearAllData();
     });
 
+    it("should add dish and return its new ID", async () => {
+        //act
+        const response = await request(baseURL).post("/dishes/").send(newDish);
+
+        //assert
+        expect(response.statusCode).toBe(201);
+        expect(response.type).toEqual('application/json')
+        // just to see it under debugger
+        let json_resp = JSON.stringify(response.body)
+        // looks like: {"id": "6318bff0fb26f4ec403e7959"}
+        expect(response.body).toEqual({id: expect.stringMatching(/^[0-9a-f]+/)})
+    });
+});
+
+describe("PUT /dishes/:id", () => {
+
+    afterAll(async () => {
+        await clearAllData();
+    });
+
+    it("should update dish for existing ID", async () => {
+        //assume
+        const dish1: Dish = {
+            Name: "ogórkowa",
+            Price: 5,
+            Category : "zupy"
+        }
+        const response1 = await request(baseURL).post("/dishes/").send(dish1);
+        const dish_id = response1.body.id
+
+        //act
+        const changed_dish: Dish = {
+            Name: "grzybowa",
+            Price: 15,
+            Category: "zupy"
+        }
+        const response = await request(baseURL).put(`/dishes/${dish_id}`).send(changed_dish);
+
+        //assert
+        expect(response.statusCode).toBe(204);
+        const response2 = await request(baseURL).get(`/dishes/${dish_id}`)
+        expect(response2.body).toEqual(expect.objectContaining(
+          {
+            _id: dish_id,
+            Name: "grzybowa",
+            Price: 15,
+            Category: "zupy"
+          }
+        ))
+    });
+});
+
+describe("DELETE /dishes/:id", () => {
+
+    afterAll(async () => {
+        await clearAllData();
+    });
+
+    it("should delete dish for existing ID", async () => {
+        //assume
+        const dish1: Dish = {
+            Name: "ogórkowa",
+            Price: 5,
+            Category : "zupy"
+        }
+        const response1 = await request(baseURL).post("/dishes/").send(dish1);
+        const dish_id = response1.body.id
+
+        //act
+        const response = await request(baseURL).delete(`/dishes/${dish_id}`);
+
+        //assert
+        expect(response.statusCode).toBe(204);
+        const response2 = await request(baseURL).get(`/dishes/${dish_id}`)
+        expect(response2.statusCode).toBe(404);
+    });
 });
